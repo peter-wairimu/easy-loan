a071f5bd1207b27ace03ed702238846eecbfad52
peter-wairimu <peter.wairimu@student.moringaschool.com>
2022-12-06 12:28:48 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+EasyLoan 
+
+its a simple loan management system where an attacker takes out a flash loan from a DeFi protocol, uses the capital that they've borrowed, and pays it back in the same transaction.
--- contracts/LoanReciever.sol
+++ contracts/LoanReciever.sol
@@ -1,2 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
+
+interface LoanReceiverV1 {
+    function executeOperation(address _reserve, uint256 _amount, uint256 _fee, bytes calldata _params) external;
+}
